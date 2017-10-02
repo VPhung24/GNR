e567ceb28ff2bc52b5f6e319f1947b4e4fdaeaa7
Vivian Phung <VPhung11@gmail.com>
2017-10-02 12:06:10 -0700

    looping

--- flask_app.py
+++ flask_app.py
@@ -14,74 +14,27 @@ def main_route():
         location_user = request.form['location']
         type_user = request.form['type']
         v = yelp_f.query_api(type_user, location_user)
+        restroom = []
+        restroom_url = []
+        restroom_image = []
+        restroom_review = []
+        restroom_rating = []
+        restroom_address = []
+        restroom_city = []
+        restroom_zipcode = []
+        restroom_state = []
+        for i in range(0, len(v["businesses"])):
+            restroom.append(v["businesses"][i]["name"])
+            restroom_url.append(v["businesses"][i]["url"])
+            restroom_image.append(v["businesses"][i]["image_url"])
+            restroom_review.append(v["businesses"][i]["review_count"])
+            restroom_rating.append(v["businesses"][i]["rating"])
+            restroom_address.append(v["businesses"][i]["location"]["address1"])
+            restroom_city.append(v["businesses"][i]["location"]["city"])
+            restroom_zipcode.append(v["businesses"][i]["location"]["zip_code"])
+            restroom_state.append(v["businesses"][i]["location"]["state"])
 
-        # restroom1
-        restroom1 = v["businesses"][0]["name"]
-        restroom1_url = v["businesses"][0]["url"]
-        restroom1_image = v["businesses"][0]["image_url"]
-        restroom1_review = v["businesses"][0]["review_count"]
-        restroom1_rating = v["businesses"][0]["rating"]
-        restroom1_address = v["businesses"][0]["location"]["address1"]
-        restroom1_city = v["businesses"][0]["location"]["city"]
-        restroom1_zipcode = v["businesses"][0]["location"]["zip_code"]
-        restroom1_state = v["businesses"][0]["location"]["state"]
-
-        # restroom2
-        restroom2 = v["businesses"][1]["name"]
-        restroom2_url = v["businesses"][1]["url"]
-        restroom2_image = v["businesses"][1]["image_url"]
-        restroom2_review = v["businesses"][1]["review_count"]
-        restroom2_rating = v["businesses"][1]["rating"]
-        restroom2_address = v["businesses"][1]["location"]["address1"]
-        restroom2_city = v["businesses"][1]["location"]["city"]
-        restroom2_zipcode = v["businesses"][1]["location"]["zip_code"]
-        restroom2_state = v["businesses"][1]["location"]["state"]
-
-        # restroom3
-        restroom3 = v["businesses"][2]["name"]
-        restroom3_url = v["businesses"][2]["url"]
-        restroom3_image = v["businesses"][2]["image_url"]
-        restroom3_review = v["businesses"][2]["review_count"]
-        restroom3_rating = v["businesses"][2]["rating"]
-        restroom3_address = v["businesses"][2]["location"]["address1"]
-        restroom3_city = v["businesses"][2]["location"]["city"]
-        restroom3_zipcode = v["businesses"][2]["location"]["zip_code"]
-        restroom3_state = v["businesses"][2]["location"]["state"]
-
-        # restroom4
-        restroom4 = v["businesses"][3]["name"]
-        restroom4_url = v["businesses"][3]["url"]
-        restroom4_image = v["businesses"][3]["image_url"]
-        restroom4_review = v["businesses"][3]["review_count"]
-        restroom4_rating = v["businesses"][3]["rating"]
-        restroom4_address = v["businesses"][3]["location"]["address1"]
-        restroom4_city = v["businesses"][3]["location"]["city"]
-        restroom4_zipcode = v["businesses"][3]["location"]["zip_code"]
-        restroom4_state = v["businesses"][3]["location"]["state"]
-
-        # restroom5
-        restroom5 = v["businesses"][4]["name"]
-        restroom5_url = v["businesses"][4]["url"]
-        restroom5_image = v["businesses"][4]["image_url"]
-        restroom5_review = v["businesses"][4]["review_count"]
-        restroom5_rating = v["businesses"][4]["rating"]
-        restroom5_address = v["businesses"][4]["location"]["address1"]
-        restroom5_city = v["businesses"][4]["location"]["city"]
-        restroom5_zipcode = v["businesses"][4]["location"]["zip_code"]
-        restroom5_state = v["businesses"][4]["location"]["state"]
-
-        # restroom6
-        restroom6 = v["businesses"][5]["name"]
-        restroom6_url = v["businesses"][5]["url"]
-        restroom6_image = v["businesses"][5]["image_url"]
-        restroom6_review = v["businesses"][5]["review_count"]
-        restroom6_rating = v["businesses"][5]["rating"]
-        restroom6_address = v["businesses"][5]["location"]["address1"]
-        restroom6_city = v["businesses"][5]["location"]["city"]
-        restroom6_zipcode = v["businesses"][5]["location"]["zip_code"]
-        restroom6_state = v["businesses"][5]["location"]["state"]
-
-        return render_template("index.html", restroom1 = restroom1, restroom1_url = restroom1_url, restroom1_image = restroom1_image, restroom1_review = restroom1_review, restroom1_rating = restroom1_rating, restroom1_address = restroom1_address, restroom1_city = restroom1_city, restroom1_zipcode = restroom1_zipcode, restroom1_state = restroom1_state, restroom2 = restroom2, restroom2_url = restroom2_url, restroom2_image = restroom2_image, restroom2_review = restroom2_review, restroom2_rating = restroom2_rating, restroom2_address = restroom2_address, restroom2_city = restroom2_city, restroom2_zipcode = restroom2_zipcode, restroom2_state = restroom2_state, restroom3 = restroom3, restroom3_url = restroom3_url, restroom3_image = restroom3_image, restroom3_review = restroom3_review, restroom3_rating = restroom3_rating, restroom3_address = restroom3_address, restroom3_city = restroom3_city, restroom3_zipcode = restroom3_zipcode, restroom3_state = restroom3_state, restroom4 = restroom4, restroom4_url = restroom4_url, restroom4_image = restroom4_image, restroom4_review = restroom4_review, restroom4_rating = restroom4_rating, restroom4_address = restroom4_address, restroom4_city = restroom4_city, restroom4_zipcode = restroom4_zipcode, restroom4_state = restroom4_state, restroom5 = restroom5, restroom5_url = restroom5_url, restroom5_image = restroom5_image, restroom5_review = restroom5_review, restroom5_rating = restroom5_rating, restroom5_address = restroom5_address, restroom5_city = restroom5_city, restroom5_zipcode = restroom5_zipcode, restroom5_state = restroom5_state, restroom6 = restroom6, restroom6_url = restroom6_url, restroom6_image = restroom6_image, restroom6_review = restroom6_review, restroom6_rating = restroom6_rating, restroom6_address = restroom6_address, restroom6_city = restroom6_city, restroom6_zipcode = restroom6_zipcode, restroom6_state = restroom6_state, found = found)
+        return render_template("index.html", restroom0 = restroom[0], restroom_url0 = restroom_url[0], restroom_image0 = restroom_image[0], restroom_review0 = restroom_review[0], restroom_rating0 = restroom_rating[0], restroom_address0 = restroom_address[0], restroom_city0 = restroom_city[0], restroom_zipcode0 = restroom_zipcode[0], restroom_state0 = restroom1_state[0], restroom1 = restroom[1], restroom_url1 = restroom_url[1], restroom_image1 = restroom_image[1], restroom_review1 = restroom_review[1], restroom_rating1 = restroom_rating[1], restroom_address1 = restroom_address[1], restroom_city1 = restroom_city[1], restroom_zipcode1 = restroom_zipcode[1], restroom_state1 = restroom_state[1], restroom2 = restroom[2], restroom_url2 = restroom_url[2], restroom_image2 = restroom_image[2], restroom_review2 = restroom_review[2], restroom_rating2 = restroom_rating[2], restroom_address2 = restroom_address[2], restroom_city2 = restroom_city[2], restroom_zipcode2 = restroom_zipcode[2], restroom_state2 = restroom_state[2], restroom3 = restroom[3], restroom_url3 = restroom_url[3], restroom_image3 = restroom_image[3], restroom_review3 = restroom_review[3], restroom_rating3 = restroom_rating[3], restroom_address3 = restroom_address[3], restroom_city3 = restroom_city[3], restroom_zipcode3 = restroom_zipcode[3], restroom_state3 = restroom_state[3], restroom4 = restroom[4], restroom_url4 = restroom_url[4], restroom_image4 = restroom_image[4], restroom_review4 = restroom_review[4], restroom_rating4 = restroom_rating[4], restroom_address4 = restroom_address[4], restroom_city4 = restroom_city[4], restroom_zipcode4 = restroom_zipcode[4], restroom_state4 = restroom_state[4], restroom5 = restroom[5], restroom_url5 = restroom_url[5], restroom_image5 = restroom_image[5], restroom_review5 = restroom_review[5], restroom_rating5 = restroom_rating[5], restroom_address5 = restroom_address[5], restroom_city5 = restroom_city[5], restroom_zipcode5 = restroom_zipcode[5], restroom_state5 = restroom_state[5], found = found)
     found = False
     return render_template("index.html", found = found)
 
