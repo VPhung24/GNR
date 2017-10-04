3b380203066eda90433d757e613465b248243b7c
Vivian Phung <VPhung11@gmail.com>
2017-10-04 13:48:58 -0700

    trying PyZip

--- flask_app.py
+++ flask_app.py
@@ -5,7 +5,6 @@ from functools import wraps
 import yelp_f
 import config
 from http import *
-from pyzipcode import Pyzipcode as pz
 
 app = Flask(__name__, template_folder='templates')
 app.config["DEBUG"] = True
